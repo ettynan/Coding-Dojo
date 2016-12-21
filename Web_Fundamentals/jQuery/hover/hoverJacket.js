d7d9e8a97cf1f95ece48648650612b176d20d759
John Doe <johndoe@example.com>
2016-12-21 08:14:43 -0800

    Adding Ninja cat exercise.

--- Web_Fundamentals/jQuery/hover/hoverJacket.js
+++ Web_Fundamentals/jQuery/hover/hoverJacket.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $('img').click(function(){
+    $('img').hover(function(){
         var temp = $(this).attr('src');
         var altnew = $(this).attr('data-alt-src');
         $(this).attr('src', altnew);
